f44d8cec8f8847b4af4ba0c63f0bcc4e1a88ece6
silnith <silnith@gmail.com>
2025-11-06 07:02:36 -0600

    Use unique_ptr = nullptr instead of reset().

--- win32/spinning-wings/SpinningWings.cpp
+++ win32/spinning-wings/SpinningWings.cpp
@@ -297,7 +297,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 	case WM_DESTROY:
 	{
-		wingsView.reset();
+		wingsView = nullptr;
 
 		// window about to be destroyed
 		HDC const hdc{ GetDC(hWnd) };
