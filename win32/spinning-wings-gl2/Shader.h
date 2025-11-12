5f6bda22cf079a9fa3c5a1d60df89b2631e6eb65
silnith <silnith@gmail.com>
2025-11-12 09:00:52 -0600

    Move the utility GLSL function definitions into the Shader class.

--- win32/spinning-wings-gl2/Shader.h
+++ win32/spinning-wings-gl2/Shader.h
@@ -20,6 +20,43 @@ namespace silnith::wings::gl2
     class Shader
     {
     public:
+        /// <summary>
+        /// The GLSL function declaration for <c>rotate</c>, which returns a
+        /// transformation matrix that rotates around an axis.
+        /// </summary>
+        static std::string const rotateMatrixFunctionDeclaration;
+
+        /// <summary>
+        /// The GLSL function definition for <c>rotate</c>, which returns a
+        /// transformation matrix that rotates around an axis.
+        /// </summary>
+        static std::string const rotateMatrixFunctionDefinition;
+
+        /// <summary>
+        /// The GLSL function declaration for <c>translate</c>, which returns a
+        /// transformation matrix that translates.
+        /// </summary>
+        static std::string const translateMatrixFunctionDeclaration;
+
+        /// <summary>
+        /// The GLSL function definition for <c>translate</c>, which returns a
+        /// transformation matrix that translates.
+        /// </summary>
+        static std::string const translateMatrixFunctionDefinition;
+
+        /// <summary>
+        /// The GLSL function declaration for <c>scale</c>, which returns a
+        /// transformation matrix that scales.
+        /// </summary>
+        static std::string const scaleMatrixFunctionDeclaration;
+
+        /// <summary>
+        /// The GLSL function definition for <c>scale</c>, which returns a
+        /// transformation matrix that scales.
+        /// </summary>
+        static std::string const scaleMatrixFunctionDefinition;
+
+    public:
         Shader(void) noexcept = delete;
         Shader(Shader const&) noexcept = delete;
         Shader& operator=(Shader const&) noexcept = delete;
--- win32/spinning-wings-gl2/WingsViewGL2.cpp
+++ win32/spinning-wings-gl2/WingsViewGL2.cpp
@@ -82,49 +82,6 @@ namespace silnith::wings::gl2
 
 		if (glInfo.isAtLeastVersion(2, 1))
 		{
-			std::string const rotateMatrixFunctionDeclaration{
-				R"shaderText(
-mat4 rotate(in float angle, in vec3 axis) {
-    // OpenGL has always specified angles in degrees.
-    // Trigonometric functions operate on radians.
-    float c = cos(radians(angle));
-    float s = sin(radians(angle));
-
-    mat3 initial = outerProduct(axis, axis)
-                   * (1 - c);
-    mat3 c_part = mat3(c);
-    mat3 s_part = mat3(0, axis.z, -axis.y,
-                       -axis.z, 0, axis.x,
-                       axis.y, -axis.x, 0)
-                  * s;
-    mat3 temp = initial + c_part + s_part;
-
-    mat4 rotation = mat4(1.0);
-    rotation[0].xyz = temp[0];
-    rotation[1].xyz = temp[1];
-    rotation[2].xyz = temp[2];
-
-    return rotation;
-}
-)shaderText"
-			};
-			std::string const translateMatrixFunctionDeclaration{
-				R"shaderText(
-mat4 translate(in vec3 move) {
-    mat4 trans = mat4(1.0);
-    trans[3].xyz = move;
-    return trans;
-}
-)shaderText"
-			};
-			std::string const scaleMatrixFunctionDeclaration{
-				R"shaderText(
-mat4 scale(in vec3 factor) {
-    return mat4(vec4(factor, 1));
-}
-)shaderText"
-			};
-
 			glslProgram = std::make_unique<Program>(
 				VertexShader{
 					R"shaderText(#version 120
@@ -202,8 +159,8 @@ void main() {
     gl_Position = gl_ModelViewProjectionMatrix * deltaTransformation * wingTransformation * gl_Vertex;
 }
 )shaderText",
-					rotateMatrixFunctionDeclaration,
-					translateMatrixFunctionDeclaration,
+					Shader::rotateMatrixFunctionDeclaration,
+					Shader::translateMatrixFunctionDeclaration,
 				},
 				FragmentShader{
 					R"shaderText(#version 120
